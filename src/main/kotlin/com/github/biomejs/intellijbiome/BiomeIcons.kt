4002bb5daf613bd9c6bc926f1b3ab580c76f2813
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-06 01:31:40 +0300

    squash!

--- src/main/kotlin/com/github/biomejs/intellijbiome/BiomeIcons.kt
+++
@@ -1,8 +0,0 @@
-package com.github.biomejs.intellijbiome
-
-import com.intellij.openapi.util.IconLoader
-
-object BiomeIcons {
-    @JvmField
-    val BiomeIcon = IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
-}
